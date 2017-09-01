bf325ca29d819e9d8fda04e645494f40bbcf869b
Andreas Reichel <webmaster@6th-dimension.com>
2017-09-01 23:13:04 +0200

    Add gtk graphics window
    Add skeleton for graphical output in X

--- sim.c
+++ sim.c
@@ -13,6 +13,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <argp.h>
+
+#include <gtk/gtk.h>
+
 #include "6502.h"
 
 #define F_CYC_NSEC 1000
@@ -100,9 +103,103 @@ void *timer_func(void *threadid)
     }
 }
 
+
+static cairo_surface_t *surface = NULL;
+
+
+static void close_window(void)
+{
+    if (surface) {
+        cairo_surface_destroy(surface);
+    }
+}
+
+static void clear_surface()
+{
+    cairo_t *cr;
+
+    cr = cairo_create(surface);
+
+    cairo_set_source_rgb(cr, 1, 1, 1);
+    cairo_paint(cr);
+
+    cairo_destroy(cr);
+}
+
+static gboolean draw_cb(GtkWidget *widget, cairo_t *cr, gpointer data)
+{
+/*    cairo_set_source_surface(cr, surface, 0, 0);
+    cairo_paint(cr);
+*/
+    cairo_set_source_rgb(cr, 0, 0, 1);
+    cairo_set_line_width(cr, 8);
+    cairo_rectangle(cr, 0, 0, 210, 156);
+    cairo_stroke_preserve(cr);
+    cairo_fill(cr);
+
+    return FALSE;
+}
+
+static gboolean configure_cb(GtkWidget *widget, GdkEventConfigure *event, gpointer data)
+{
+    if (surface) {
+        cairo_surface_destroy(surface);
+    }
+
+    surface = gdk_window_create_similar_surface(gtk_widget_get_window(widget),
+                                                CAIRO_CONTENT_COLOR,
+                                                gtk_widget_get_allocated_width(widget),
+                                                gtk_widget_get_allocated_height(widget));
+
+    clear_surface();
+
+    return TRUE;
+}
+
+void initMainWindow()
+{
+    GtkWidget *main_window;
+
+    gtk_init(0, NULL);
+
+    main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(main_window), "RGA Emulation Window");
+    gtk_window_set_default_size(GTK_WINDOW(main_window), 420, 312);
+
+    g_signal_connect(main_window, "destroy", G_CALLBACK(close_window), NULL);
+    gtk_container_set_border_width(GTK_CONTAINER(main_window), 8);
+
+    GtkWidget *frame = gtk_frame_new(NULL);
+    gtk_frame_set_shadow_type(GTK_FRAME(frame), GTK_SHADOW_IN);
+    gtk_container_add(GTK_CONTAINER(main_window), frame);
+
+
+    GtkWidget *drawing_area = gtk_drawing_area_new();
+    gtk_widget_set_size_request(drawing_area, 420, 312);
+
+    gtk_container_add(GTK_CONTAINER(frame), drawing_area);
+
+    g_signal_connect(drawing_area, "draw", G_CALLBACK(draw_cb), NULL);
+    //g_signal_connect(drawing_area, "configure-event", G_CALLBACK(configure_cb), NULL);
+
+    gtk_widget_show_all(main_window);
+}
+
+void *gtk_main_func(void *data)
+{
+    while(sim_running) {
+        if (gtk_events_pending()) {
+            gtk_main_iteration();
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
 int main(int argc, char **argv)
 {
     pthread_t timer_thread;
+    pthread_t gtk_thread;
 
     struct arguments arguments;
 
@@ -118,20 +215,20 @@ int main(int argc, char **argv)
     uint8_t op, cycles;
     struct timespec time;
 
+    initMainWindow();
+
     /* Get timer resolution */
     clock_getres(CLOCK_REALTIME, &time);
     fprintf(stdout, "Realtime clock resolution: %lu ns\n", time.tv_nsec);
 
     /* Callibrate time delay */
-    
-    // 
     struct timespec oldtime;
     clock_gettime(CLOCK_REALTIME, &oldtime);
     for (long j = 0; j < 5E8; j++) {
         asm("nop;nop;nop;nop;nop;nop;nop;nop;nop;nop");
     }
     clock_gettime(CLOCK_REALTIME, &time);
-    
+
     long delta_ns = time.tv_nsec - oldtime.tv_nsec;
     long delta_s = time.tv_sec - oldtime.tv_sec;
     double delta = (double) delta_s + 1.0E-9 * delta_ns;
@@ -144,34 +241,43 @@ int main(int argc, char **argv)
     printf("clock_loop = %lu\n", clock_loop);
     clock_loop_times = clock_loop * 10;
 
+    /* Initialize stack pointer */
     cpu.S = 0xFF;
-    cpu.P = 0x20;   // undefined bit is always set
 
+    /* Initialize processor status (bit 5 is always 1) */
+    cpu.P = 0x20;
+
+    /* Read ROM from file */
     FILE *romfile = fopen(arguments.romfile, "r");
     if (!romfile) {
         fprintf(stderr, "Could not load ROM from file.\n");
         exit(1);
-    }  
-
+    }
     size_t res = fread(&memory[arguments.romaddr], 1, 0xFFFF - arguments.romaddr + 1, romfile);
-    
     fclose(romfile);
-    
     fprintf(stdout, "ROM: %u bytes read.\n", res);
 
+    /* Check if ROM has correct size and if vectors are present */
     if (res != 0xFFFF - arguments.romaddr + 1) {
         fprintf(stderr, "Your ROM file is too small, no data at vector table.\n");
         exit(1);
     }
-    /* Reset Vector */
+
+    /* Fetch reset Vector */
     uint16_t start_addr = *((uint16_t *) &memory[0xFFFC]);
     cpu.PC = start_addr;
 
     /* Create timer thread */
-
     int rc = pthread_create(&timer_thread, NULL, timer_func, NULL);
     if (rc) {
         fprintf(stderr, "Cannot create timer thread.\n");
+        exit(-1);
+    }
+
+    /* Create gtk thread */
+    rc = pthread_create(&gtk_thread, NULL, gtk_main_func, NULL);
+    if (rc) {
+        fprintf(stderr, "Cannot create gtk thread.\n");
         exit(-1);
     }
 
@@ -179,7 +285,7 @@ int main(int argc, char **argv)
     long cdelta;
     while(cpu.PC != 0xFFFF) {
         clock_gettime(CLOCK_REALTIME, &time);
-        
+
         /* fine tuning */
         cdelta = time.tv_nsec - oldns;
         if (cdelta > 0) {
@@ -192,24 +298,50 @@ int main(int argc, char **argv)
         }
         oldns = time.tv_nsec;
         printf("\n%lu,%09lu - ", time.tv_sec, time.tv_nsec);
+
+        // fetch the opcode from memory
         op = memory[cpu.PC];
         printf("%04X : %02X ", cpu.PC, op);
+
+        // each opcode can be understood as aaabbbcc, where
+        // bbb gives the addressing mode and ccaaa the instruction
+
+        // extract cc
         cc = op & 3;
+
+        // rol by 2 (opcode becomes ccaaabbb)
         op >>= 2;
         op |= cc << 6;
+
+        // exctract instruction selector (ccaaa)
         idx = op >> 3;
+
+        // idx specifies instruction, op & 7 is bbb, which gives
+        // adressing mode
         a = opcodes[idx].addr_modes[op & 7];
+
+        // bbb also gives number of cycles
         cycles = opcodes[idx].cycles[op & 7];
         printf(" CYC = %u ", cycles);
+
+        // calculate needed timing loop counts
         clock_loop_times = clock_loop * cycles;
+
         if (arguments.debug_regs) {
             dumpRegs();
         }
+
+        // signal the timing thread that it can start counting
         wait_clock = true;
+
+        // parallely, execute CPU instruction
         opcodes[idx].func(a, op & 7);
-        // wait cycles
+
+        // wait remaining counts done by timing thread
         while(wait_clock);
     }
+
+    // signal the timing thread that it must stop
     sim_running = false;
 
     pthread_exit(NULL);
